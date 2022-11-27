a54a18e7e6dc0cab615da6cdcf388a40f88ae8ca
Эльдар Букеев <bukeevel@gmail.com>
2022-11-27 14:05:32 +0300

    added Prism app

---
+++ CompanyAnalyzerWpf/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CompanyAnalyzerWpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
